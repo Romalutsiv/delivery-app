752e3055de6f881f1a08d186d7d1590c15284060
Roma <lutsiv55@gmail.com>
2024-07-24 22:16:04 +0300

    Create empty workers service.

---
+++ workers-service/src/main/java/org/workers/workersservice/WorkersServiceApp.java
@@ -0,0 +1,11 @@
+package org.workers.workersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkersServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkersServiceApp.class, args);
+    }
+}
